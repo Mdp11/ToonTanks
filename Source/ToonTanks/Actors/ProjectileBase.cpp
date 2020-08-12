79ab46c0effe7300ae7896ff4a06fbd7bb7ef126
Mattia De Prisco <mattia.dipi@gmail.com>
2020-08-12 20:55:28 +0200

    Add projectil base actor and derived bp

---
+++ Source/ToonTanks/Actors/ProjectileBase.cpp
@@ -0,0 +1,28 @@
+// Mattia De Prisco 2020
+
+#include "ProjectileBase.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+// Sets default values
+AProjectileBase::AProjectileBase()
+{
+    // Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = false;
+
+    ProjectileMesh = CreateDefaultSubobject<UStaticMeshComponent>(
+        TEXT("Projectile Mesh"));
+    RootComponent = ProjectileMesh;
+    
+    ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(
+        TEXT("Projectile Movement"));
+
+    ProjectileMovement->InitialSpeed = ProjectileMovement->MaxSpeed = MovementSpeed;
+
+    InitialLifeSpan = 3.0f;
+}
+
+// Called when the game starts or when spawned
+void AProjectileBase::BeginPlay()
+{
+    Super::BeginPlay();
+}
---
+++ Source/ToonTanks/Actors/ProjectileBase.h
@@ -0,0 +1,43 @@
+// Mattia De Prisco 2020
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ProjectileBase.generated.h"
+
+class UProjectileMovementComponent;
+
+UCLASS()
+class TOONTANKS_API AProjectileBase : public AActor
+{
+    GENERATED_BODY()
+private:
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components",
+        meta=(AllowPrivateAccess="true"))
+    UProjectileMovementComponent* ProjectileMovement{nullptr};
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components",
+        meta=(AllowPrivateAccess="true"))
+    UStaticMeshComponent* ProjectileMesh{nullptr};
+
+    UPROPERTY(EditDefaultsOnly, Category="Damage")
+    TSubclassOf<UDamageType> DamageType;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Movement",
+        meta=(AllowPrivateAccess="true"))
+    float MovementSpeed{1300.f};
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Damage",
+        meta=(AllowPrivateAccess="true"))
+    float Damage{15.f};
+
+public:
+    // Sets default values for this actor's properties
+    AProjectileBase();
+
+protected:
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
+};
