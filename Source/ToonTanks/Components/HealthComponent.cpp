adeed45c494460d10478bf12b1d867d78802d8b5
Mattia De Prisco <mattia.dipi@gmail.com>
2020-08-22 12:00:19 +0200

    Add health functionality

---
+++ Source/ToonTanks/Components/HealthComponent.cpp
@@ -0,0 +1,51 @@
+// Mattia De Prisco 2020
+
+#include "HealthComponent.h"
+#include "ToonTanks/GameModes/TanksGameModeBase.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values for this component's properties
+UHealthComponent::UHealthComponent()
+{
+    // Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+    // off to improve performance if you don't need them.
+    PrimaryComponentTick.bCanEverTick = false;
+
+    // ...
+}
+
+// Called when the game starts
+void UHealthComponent::BeginPlay()
+{
+    Super::BeginPlay();
+
+    Health = DefaultHealth;
+    GameModeRef = Cast<ATanksGameModeBase>(UGameplayStatics::GetGameMode
+        (GetWorld()));
+
+    GetOwner()->OnTakeAnyDamage.AddDynamic(this, &UHealthComponent::TakeDamage);
+}
+
+void UHealthComponent::TakeDamage(AActor* DamagedActor, float Damage,
+                                  const UDamageType* DamageType,
+                                  AController* InstigatedBy, AActor* DamagedBy)
+{
+    if(Damage == 0.f || Health <= 0.f)
+    {
+        return;
+    }
+
+    Health = FMath::Clamp(Health - Damage, 0.f, DefaultHealth);
+
+    if(Health <= 0.f)
+    {
+        if(GameModeRef)
+        {
+            GameModeRef->ActorDied(GetOwner());
+        }
+        else
+        {
+            UE_LOG(LogTemp, Warning, TEXT("HealthComponent: Pointer to GameMode not set"));
+        }
+    }
+}
